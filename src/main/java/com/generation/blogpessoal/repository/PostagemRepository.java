7cda21cc6fd1e5ea0a7d78ec4679215ecbe8b0d5
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-04-14 19:38:00 -0300

    atualização 14/04

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,6 +3,7 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -11,6 +12,6 @@ import com.generation.blogpessoal.model.Postagem;
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 	List<Postagem> findAll();
-
-
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
